b5f643aa7139b9370e0ce17bf964a1b110b83071
Jared Lewis <jared@jared.kiwi.nz>
2024-05-13 12:11:31 +0200

    chore: Base on ubuntu instead of wrf

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,20 @@
-FROM ghcr.io/climate-resource/wrf:4.5.1
+FROM ubuntu:20.04
 
 MAINTAINER Jared Lewis <jared.lewis@climate-resource.com>
 
 ARG DEBIAN_FRONTEND=noninteractive
 ARG CMAQ_VERSION="5.0.2"
+ARG CMAQ_BIN="Linux2_armgfort"
 ENV TZ=Etc/UTC
 
-WORKDIR /opt/cmaq
-COPY scripts templates /opt/cmaq/
+RUN apt-get update && \
+    apt-get install -y build-essential gfortran m4 csh git jq wget libmpich-dev libnetcdff-dev && \
+    rm -rf /var/lib/apt/lists/*
 
-RUN CMAQ_VERSION=${CMAQ_VERSION} bash /opt/cmaq/scripts/build_cmaq.sh
+WORKDIR /opt/cmaq
+COPY templates /opt/cmaq/templates/
+COPY scripts/build_ioapi.sh /opt/cmaq/scripts/build_ioapi.sh
+
+RUN ls /opt/cmaq && CMAQ_VERSION=${CMAQ_VERSION} BIN=${CMAQ_BIN} bash /opt/cmaq/scripts/build_ioapi.sh
 
 ENTRYPOINT ["/bin/bash"]
--- scripts/build_ioapi.sh
+++ scripts/build_ioapi.sh
@@ -5,13 +5,18 @@ set -x
 set -e
 
 IOAPI_VERSION="3.2"
-CMAQ_VERSION=${-CMAQ_VERSION:"5.0.2"}
+CMAQ_VERSION=${CMAQ_VERSION:-"5.0.2"}
 DIR=/opt/wrf/libs
 
 # Link to the compiled dependencies from the WRF container
 export PATH=$DIR/bin:$PATH
 export LDFLAGS=-L$DIR/lib
 export CPPFLAGS=-I$DIR/include
+
+# Setup the build environment for iotools
+export BIN=${BIN:-Linux2_x86_64gfort}
+export BASEDIR=$PWD
+export CPLMODE=nocpl
 
 # Build IOAPI
 wget -nv https://www.cmascenter.org/ioapi/download/ioapi-${IOAPI_VERSION}.tar.gz  -O ioapi-v${IOAPI_VERSION}.tar.gz
@@ -21,15 +26,15 @@ pushd ioapi-v${IOAPI_VERSION} || exit
 
 # Copy in Makefile template
 # This uses the correct configuration for the built NetCDF library
-cp /opt/wrf/build/templates/ioapi/Makefile Makefile
-cp ioapi/Makefile.nocpl ioapi/Makefile # This is overridden by the .nocpl.sed template
-cp m3tools/Makefile.nocpl m3tools/Makefile  # This is overridden by the .nocpl.sed template
+cp /opt/cmaq/templates/ioapi/Makefile Makefile
+cp /opt/cmaq/templates/ioapi/Makeinclude.Linux2_armgfort ioapi/
+# The following makefiles will be overridden by the .nocpl.sed templates
+# but need to exist for the configure step to work
+touch ioapi/Makefile m3tools/Makefile
 
-# Setup the build environment
-export BIN=Linux2_x86_64gfort10
-export BASEDIR=$PWD
-export CPLMODE=nocpl
 
+
+make configure
 make
 
 # build CMAQ
