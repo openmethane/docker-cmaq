62cf2d2392d9b82194fc27a5c75504c2f849bd86
Jared Lewis <jared@jared.kiwi.nz>
2024-06-05 21:51:26 +0300

    chore: Run adj afterwards

--- scripts/build_11_cmaq_adj.sh
+++ scripts/build_11_cmaq_adj.sh
@@ -22,8 +22,9 @@ fi
 # Build the Forward adjoint model
 csh bldit.adjoint.fwd.CH4only
 
-if [[ ! -f /opt/cmaq/cmaq_adj/BLD_bwd_CH4only/ADJOINT_BWD ]]; then
+if [[ ! -f /opt/cmaq/cmaq_adj/BLD_fwd_CH4only/ADJOINT_FWD ]]; then
     echo "Forward adjoint failed to build"
     exit 1
 fi
 
+/opt/cmaq/cmaq_adj/BLD_fwd_CH4only/ADJOINT_FWD 2>&1 | tee -a adj_fwd.log
